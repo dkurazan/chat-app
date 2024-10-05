33baff4f4e1942be32457a7553b29d33af029111
Dmytro <deekorde@gmail.com>
2024-10-05 12:39:33 +0300

    minor changes

---
+++ README.md
@@ -0,0 +1,33 @@
+# Chat application
+
+A chat application implemented using following technologies: 
+ - React
+ - Node.js
+ - Express
+ - MongoDB
+ - Socket.io
+ - Zustand
+ - React Router
+ - Tailwind
+ - Daisy UI
+
+# Live Demo
+
+Check out the live demo at
+[*](*)
+
+## Requirements
+
+Before getting started, make sure you have:
+
+- Node.js
+- npm or Yarn
+
+## Installation:
+1. Clone the project repository.
+2. Run the command `npm run build` to install the necessary dependencies and to build frontend.
+3. Create a .env file in the root directory of the project and define the environment variables following the descriptions provided in .env.example.
+4. Execute `npm start` to start the project.
+5. Open a web browser and go to
+   [http://http://localhost:5000/](http://http://localhost:5000/)
+   to view the application.
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -7,11 +8,13 @@ import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser()); 
@@ -20,11 +23,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send('Hello world!!!')
-// })
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.listen(PORT, () => {
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on port ${PORT}`);
 });
